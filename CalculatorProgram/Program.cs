e1aa715e77b960b57e52ead2ac7d8486f522c686
Zoltán Nyiri <zoltan.nyiri02@gmail.com>
2024-03-04 16:21:14 +0100

    Add project files.

---
+++ CalculatorProgram/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Calculator;
+
+namespace CalculatorApp
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Calculator.Calculator calculator = new Calculator.Calculator();
+
+            Console.WriteLine("Írj be két számot az összeadáshoz:");
+            if (int.TryParse(Console.ReadLine(), out int a) && int.TryParse(Console.ReadLine(), out int b))
+            {
+                int result = calculator.Add(a, b);
+                Console.WriteLine($"Összeg: {result}");
+            }
+            else
+            {
+                Console.WriteLine("Érvénytelen bemenet a számokhoz.");
+            }
+        }
+    }
+}
